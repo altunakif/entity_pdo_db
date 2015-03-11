224bf070d1c417dfe556be80719d250c3be805d3
Akif <altun_akif@hotmail.com>
2015-03-11 15:46:24 +0200

    example

---
+++ example.php
@@ -0,0 +1,14 @@
+﻿<?php
+require "entity_pdo.php";
+
+$exe = new dataBase();
+$exe ->from("haberler")
+	 ->insert(array(baslik=>"'başlık'", icerik=>"'icerik'"))
+	 ->run();
+	 
+$exe ->select()
+	 ->run();
+
+var_dump($exe->result);	 	 
+
+?>
