78c9a17b52ffcdeb89fc77083dc1aaffd94d29eb
Akif ALTUN <altun_akif@hotmail.com>
2015-05-27 12:33:47 +0300

    Update
    Update

---
+++ DirectSql.php
@@ -0,0 +1,10 @@
+﻿<?php
+include_once('DataBase.php');
+
+$select = new DataBase();
+$select -> run("UPDATE drafts SET status=1 WHERE id=195");
+
+var_dump($select->sql);
+var_dump($select->bindPar);
+var_dump($select->result);
+?>
